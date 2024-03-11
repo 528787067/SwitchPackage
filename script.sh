ae64afda42a34220afbb6a95168534af91f3eb9e
方块8 <528787067@qq.com>
2024-03-11 23:03:09 +0800

    [modify] export proxy url

--- script.sh
+++ script.sh
@@ -10,14 +10,16 @@ mkdir -p downloads
 mkdir -p packages
 mkdir -p releases
 
+export GITHUB_PROXY=https://github.moeyy.xyz/
+
 curl -sL 'https://api.github.com/repos/CTCaer/hekate/releases/latest' \
     | jq -r '.assets[] | select(.name | test("hekate_ctcaer_[.\\d]+_Nyx_[.\\d]+(_v\\d+)?.zip")) | .name, .browser_download_url' \
-    | xargs -n2 sh -c 'curl -L https://ghproxy.com/$1 -o downloads/$0'
+    | xargs -n2 sh -c 'curl -L $GITHUB_PROXY$1 -o downloads/$0'
 find 'downloads' -name 'hekate_ctcaer_*.zip' | xargs -I {} unzip -q -u -o -d 'packages' {}
 
 curl -sL 'https://api.github.com/repos/Atmosphere-NX/Atmosphere/releases' \
     | jq -r '.[0].assets[] | select(.name | test("atmosphere-[.\\d]+-(master|prerelease)-\\w+\\+hbl-[.\\d]+\\+hbmenu-[.\\d]+.zip")) | .name, .browser_download_url' \
-    | xargs -n2 sh -c 'curl -L https://ghproxy.com/$1 -o downloads/$0'
+    | xargs -n2 sh -c 'curl -L $GITHUB_PROXY$1 -o downloads/$0'
 find 'downloads' -name 'atmosphere-*.zip' | xargs -I {} unzip -q -u -o -d 'packages' {}
 
 curl -L 'https://sigmapatches.coomer.party/sigpatches.zip' -o 'downloads/sigpatches.zip'
@@ -36,7 +38,7 @@ rm -r -f packages/*-SwitchPackage-*
 
 curl -sL 'https://api.github.com/repos/Atmosphere-NX/Atmosphere/releases' \
     | jq -r '.[0].assets[] | select(.name == "fusee.bin") | .name, .browser_download_url' \
-    | xargs -n2 sh -c 'curl -L https://ghproxy.com/$1 -o downloads/$0'
+    | xargs -n2 sh -c 'curl -L $GITHUB_PROXY$1 -o downloads/$0'
 cp -f 'downloads/fusee.bin' 'packages/bootloader/payloads/fusee.bin'
 
 cd 'packages' && zip -q -r '../releases/hekate_atmosphere_sigpatches_configs.zip' . && cd ..
@@ -47,10 +49,10 @@ mkdir -p 'packages/switch/daybreak' && mv -f 'packages/switch/daybreak.nro' 'pac
 
 curl -sL 'https://api.github.com/repos/rashevskyv/dbi/releases/latest' \
     | jq -r '.assets[] | select(.name == "DBI.nro") | .name, .browser_download_url' \
-    | xargs -n2 sh -c 'curl -L https://ghproxy.com/$1 -o downloads/$0'
+    | xargs -n2 sh -c 'curl -L $GITHUB_PROXY$1 -o downloads/$0'
 curl -sL 'https://api.github.com/repos/rashevskyv/dbi/releases/latest' \
     | jq -r '.assets[] | select(.name == "dbi.config") | .name, .browser_download_url' \
-    | xargs -n2 sh -c 'curl -L https://ghproxy.com/$1 -o downloads/$0'
+    | xargs -n2 sh -c 'curl -L $GITHUB_PROXY$1 -o downloads/$0'
 mkdir -p 'packages/switch/DBI'
 cp -f 'downloads/DBI.nro' 'packages/switch/DBI/DBI.nro'
 cp -f 'downloads/dbi.config' 'packages/switch/DBI/dbi.config'
@@ -58,17 +60,17 @@ sed -i 's/ExitToHomeScreen=true/ExitToHomeScreen=false/' 'packages/switch/DBI/db
 
 curl -sL 'https://api.github.com/repos/joel16/NX-Shell/releases/latest' \
     | jq -r '.assets[] | select(.name == "NX-Shell.nro") | .name, .browser_download_url' \
-    | xargs -n2 sh -c 'curl -L https://ghproxy.com/$1 -o downloads/$0'
+    | xargs -n2 sh -c 'curl -L $GITHUB_PROXY$1 -o downloads/$0'
 mkdir -p 'packages/switch/NX-Shell' && cp -f 'downloads/NX-Shell.nro' 'packages/switch/NX-Shell/NX-Shell.nro'
 
 curl -sL 'https://api.github.com/repos/zdm65477730/Switch-Firmware-Dumper/releases/latest' \
     | jq -r '.assets[] | select(.name == "Firmware-Dumper.zip") | .name, .browser_download_url' \
-    | xargs -n2 sh -c 'curl -L https://ghproxy.com/$1 -o downloads/$0'
+    | xargs -n2 sh -c 'curl -L $GITHUB_PROXY$1 -o downloads/$0'
 unzip -q -u -o -d 'packages' 'downloads/Firmware-Dumper.zip'
 
 curl -sL 'https://api.github.com/repos/WerWolv/Hekate-Toolbox/releases/latest' \
     | jq -r '.assets[] | select(.name == "HekateToolbox.nro") | .name, .browser_download_url' \
-    | xargs -n2 sh -c 'curl -L https://ghproxy.com/$1 -o downloads/$0'
+    | xargs -n2 sh -c 'curl -L $GITHUB_PROXY$1 -o downloads/$0'
 mkdir -p 'packages/switch/HekateToolbox' && cp -f 'downloads/HekateToolbox.nro' 'packages/switch/HekateToolbox/HekateToolbox.nro'
 
 cd 'packages' && zip -q -r '../releases/hekate_atmosphere_sigpatches_configs_tools.zip' . && cd ../..
