7a7dd9a6050751918b3e00372e0a461f61715832
方块8 <528787067@qq.com>
2025-03-17 22:25:19 +0800

    [modify] use sys-patch

--- script.sh
+++ script.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+GITHUB_PROXY="$1"
+if [[ -n "$GITHUB_PROXY" && "$GITHUB_PROXY" != */ ]]; then
+    GITHUB_PROXY+="/"
+fi
+export GITHUB_PROXY
 
 set -e
 set -v on
@@ -12,39 +18,42 @@ mkdir -p releases
 
 curl -sL 'https://api.github.com/repos/CTCaer/hekate/releases/latest' \
     | jq -r '.assets[] | select(.name | test("hekate_ctcaer_[.\\d]+_Nyx_[.\\d]+(_v\\d+)?.zip")) | .name, .browser_download_url' \
-    | xargs -n2 sh -c 'curl -L $1 -o downloads/$0'
+    | xargs -n2 sh -c 'curl -L $GITHUB_PROXY$1 -o downloads/$0'
 find 'downloads' -name 'hekate_ctcaer_*.zip' | xargs -I {} unzip -q -u -o -d 'packages' {}
 
 curl -sL 'https://api.github.com/repos/Atmosphere-NX/Atmosphere/releases' \
     | jq -r '.[0].assets[] | select(.name | test("atmosphere-[.\\d]+-(master|prerelease)-\\w+\\+hbl-[.\\d]+\\+hbmenu-[.\\d]+.zip")) | .name, .browser_download_url' \
-    | xargs -n2 sh -c 'curl -L $1 -o downloads/$0'
+    | xargs -n2 sh -c 'curl -L $GITHUB_PROXY$1 -o downloads/$0'
 find 'downloads' -name 'atmosphere-*.zip' | xargs -I {} unzip -q -u -o -d 'packages' {}
 
-curl -L 'https://sigmapatches.coomer.party/sigpatches.zip' -o 'downloads/sigpatches.zip'
-unzip -q -u -o -d 'packages' 'downloads/sigpatches.zip'
+curl -sL 'https://api.github.com/repos/borntohonk/sys-patch/releases' \
+    | jq -r '.[0].assets[] | select(.name == "sys-patch.zip") | .name, .browser_download_url' \
+    | xargs -n2 sh -c 'curl -L $GITHUB_PROXY$1 -o downloads/$0'
+unzip -q -u -o -d 'packages' 'downloads/sys-patch.zip'
 
-cd 'packages' && zip -q -r '../releases/hekate_atmosphere_sigpatches.zip' . && cd ..
+cd 'packages' && zip -q -r '../releases/hekate_atmosphere_syspatch.zip' . && cd ..
 
 cp -r -f ../configs/* 'packages'
 mv -f packages/hekate_ctcaer_*.bin 'packages/payload.bin'
 
 curl -sL 'https://api.github.com/repos/Atmosphere-NX/Atmosphere/releases' \
     | jq -r '.[0].assets[] | select(.name == "fusee.bin") | .name, .browser_download_url' \
-    | xargs -n2 sh -c 'curl -L $1 -o downloads/$0'
+    | xargs -n2 sh -c 'curl -L $GITHUB_PROXY$1 -o downloads/$0'
 cp -f 'downloads/fusee.bin' 'packages/bootloader/payloads/fusee.bin'
 
-cd 'packages' && zip -q -r '../releases/hekate_atmosphere_sigpatches_configs.zip' . && cd ..
+cd 'packages' && zip -q -r '../releases/hekate_atmosphere_syspatch_configs.zip' . && cd ..
 
 rm -f 'packages/switch/haze.nro'
 rm -f 'packages/switch/reboot_to_payload.nro'
+rm -r -f 'packages/switch/.overlays'
 mkdir -p 'packages/switch/daybreak' && mv -f 'packages/switch/daybreak.nro' 'packages/switch/daybreak/daybreak.nro'
 
 curl -sL 'https://api.github.com/repos/rashevskyv/dbi/releases/latest' \
     | jq -r '.assets[] | select(.name == "DBI.nro") | .name, .browser_download_url' \
-    | xargs -n2 sh -c 'curl -L $1 -o downloads/$0'
+    | xargs -n2 sh -c 'curl -L $GITHUB_PROXY$1 -o downloads/$0'
 curl -sL 'https://api.github.com/repos/rashevskyv/dbi/releases/latest' \
     | jq -r '.assets[] | select(.name == "dbi.config") | .name, .browser_download_url' \
-    | xargs -n2 sh -c 'curl -L $1 -o downloads/$0'
+    | xargs -n2 sh -c 'curl -L $GITHUB_PROXY$1 -o downloads/$0'
 mkdir -p 'packages/switch/DBI'
 cp -f 'downloads/DBI.nro' 'packages/switch/DBI/DBI.nro'
 cp -f 'downloads/dbi.config' 'packages/switch/DBI/dbi.config'
@@ -52,17 +61,17 @@ sed -i 's/ExitToHomeScreen=true/ExitToHomeScreen=false/' 'packages/switch/DBI/db
 
 curl -sL 'https://api.github.com/repos/joel16/NX-Shell/releases/latest' \
     | jq -r '.assets[] | select(.name == "NX-Shell.nro") | .name, .browser_download_url' \
-    | xargs -n2 sh -c 'curl -L $1 -o downloads/$0'
+    | xargs -n2 sh -c 'curl -L $GITHUB_PROXY$1 -o downloads/$0'
 mkdir -p 'packages/switch/NX-Shell' && cp -f 'downloads/NX-Shell.nro' 'packages/switch/NX-Shell/NX-Shell.nro'
 
 curl -sL 'https://api.github.com/repos/zdm65477730/Switch-Firmware-Dumper/releases/latest' \
     | jq -r '.assets[] | select(.name == "Firmware-Dumper.zip") | .name, .browser_download_url' \
-    | xargs -n2 sh -c 'curl -L $1 -o downloads/$0'
+    | xargs -n2 sh -c 'curl -L $GITHUB_PROXY$1 -o downloads/$0'
 unzip -q -u -o -d 'packages' 'downloads/Firmware-Dumper.zip'
 
 curl -sL 'https://api.github.com/repos/WerWolv/Hekate-Toolbox/releases/latest' \
     | jq -r '.assets[] | select(.name == "HekateToolbox.nro") | .name, .browser_download_url' \
-    | xargs -n2 sh -c 'curl -L $1 -o downloads/$0'
+    | xargs -n2 sh -c 'curl -L $GITHUB_PROXY$1 -o downloads/$0'
 mkdir -p 'packages/switch/HekateToolbox' && cp -f 'downloads/HekateToolbox.nro' 'packages/switch/HekateToolbox/HekateToolbox.nro'
 
-cd 'packages' && zip -q -r '../releases/hekate_atmosphere_sigpatches_configs_tools.zip' . && cd ../..
+cd 'packages' && zip -q -r '../releases/hekate_atmosphere_syspatch_configs_tools.zip' . && cd ../..
